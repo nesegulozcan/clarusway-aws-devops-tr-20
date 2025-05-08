e33b63130acc295239881e09a4daa2ab8e17e3fd
Morgoliath <aslan@clarusway.com>
2025-05-08 23:48:45 +0300

    Phonebook app yaml and py file added

--- aws/projects/phonebook-web-app/phonebook-app.py
+++ aws/projects/phonebook-web-app/phonebook-app.py
@@ -1,9 +1,23 @@
 # Import Flask modules
 from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
+import boto3
 
-# Create an object named app
+def get_ssm_parameters():
+    ssm = boto3.client('ssm', region_name='us-east-1')
+
+    username_param = ssm.get_parameter(Name='/Morgoliath/phonebook-web-app/username')
+    password_param = ssm.get_parameter(Name="/Morgoliath/phonebook-web-app/password", WithDecryption=True)
+
+
+    username = username_param['Parameter']['Value']
+    password = password_param['Parameter']['Value']
+
+    return username, password
+
 app = Flask(__name__)
+
+db_username, db_password = get_ssm_parameters()
 
 # This "/home/ec2-user/dbserver.endpoint" file has to be created from cloudformation template and it has RDS endpoint
 db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8') 
@@ -11,8 +25,8 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 # Configure mysql database
 
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
-app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'clarusway_1234'
+app.config['MYSQL_DATABASE_USER'] = db_username
+app.config['MYSQL_DATABASE_PASSWORD'] = db_password
 app.config['MYSQL_DATABASE_DB'] = 'clarusway_phonebook'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
